2d06555efa30ee77eefbe6a206dd15ed0d73d2cc
Lucas Vanzella <lucasvanzellap2@gmail.com>
2026-08-27 23:43:51 -0300

    DLQs

--- src/main/java/com/microsservicos/demo/connections/RabbitMQConnection.java
+++ src/main/java/com/microsservicos/demo/connections/RabbitMQConnection.java
@@ -4,6 +4,7 @@ import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.stereotype.Component;
 
 import com.example.constantes.RabbitMQConstantes;
@@ -21,7 +22,18 @@ public class RabbitMQConnection {
     }
 
     private Queue fila(String nomeFila){
-        return new Queue(nomeFila, true, false, false);
+        return QueueBuilder
+                .durable(nomeFila)
+                .build();
+    }
+
+    private Queue filaEstoque(){
+    //Quando uma mensagem da fila ESTOQUE for rejeitada sem requeue, publica novamente no amq.direct, agora com a routing key ESTOQUE.DLQ   
+        return QueueBuilder
+                .durable(RabbitMQConstantes.FILA_ESTOQUE)
+                .deadLetterExchange(NOME_EXCHANGE)
+                .deadLetterRoutingKey(RabbitMQConstantes.ESTOQUE_DLQ)
+                .build();
     }
 
     private DirectExchange trocaDireta(){
@@ -34,19 +46,24 @@ public class RabbitMQConnection {
 
     @PostConstruct
     private void adiciona(){
-        Queue filaEstoque = this.fila(RabbitMQConstantes.FILA_ESTOQUE);
+        Queue filaEstoque = this.filaEstoque();
         Queue filaPreco = this.fila(RabbitMQConstantes.FILA_PRECO);
+        Queue filaEstoqueDlq = this.fila(RabbitMQConstantes.ESTOQUE_DLQ);
 
         DirectExchange troca = this.trocaDireta();
+        
         Binding ligacaoEstoque = this.relacionamento(filaEstoque, troca);
-        Binding ligacaoPreco = this.relacionamento(filaEstoque, troca);
+        Binding ligacaoPreco = this.relacionamento(filaPreco, troca);
+        Binding ligacaoEstoqueDlq = this.relacionamento(filaEstoqueDlq, troca);
 
         this.amqpAdmin.declareQueue(filaEstoque);
         this.amqpAdmin.declareQueue(filaPreco);
+        this.amqpAdmin.declareQueue(filaEstoqueDlq);
 
         this.amqpAdmin.declareExchange(troca);
         
         this.amqpAdmin.declareBinding(ligacaoEstoque);
         this.amqpAdmin.declareBinding(ligacaoPreco);
+        this.amqpAdmin.declareBinding(ligacaoEstoqueDlq);
     }
 }
